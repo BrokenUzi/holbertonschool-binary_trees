80e8693b62262750b08f17c8b2ebb32a912c6adf
root <jcup143@gmail.com>
2023-08-21 18:01:23 -0700

    saving

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_perfect - 
+* @tree: tree
+* Return: 
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+}
